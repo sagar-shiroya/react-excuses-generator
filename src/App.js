9ff2c7298c1088764df9efb37c9c5a1bd2da83a6
Sagar Shiroya <sagar.shiroya8@gmail.com>
2023-03-07 12:41:43 +0530

    Changes related to styling and excuse generator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import { Footer } from "./Footer";
 
 function App() {
+  const [excuse, setExcuse] = useState("");
+
+  const generateExcuse = (category) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}/`)
+      .then((res) => res.data[0])
+      .then((res) => setExcuse(res.excuse));
+  };
   return (
     <div className="App">
+      <h1>Excuses Generator</h1>
+      <div>
+        <button onClick={() => generateExcuse("family")}>Family</button>
+        <button onClick={() => generateExcuse("office")}>Office</button>
+        <button onClick={() => generateExcuse("college")}>College</button>
+        <button onClick={() => generateExcuse("party")}>Party</button>
+        <button onClick={() => generateExcuse("developers")}>Developers</button>
+        <button onClick={() => generateExcuse("funny")}>Funny</button>
+      </div>
+      <h2>{excuse}</h2>
+      <Footer />
     </div>
   );
 }
